05d765182ec8f6774c1e973387268eee8454c8ac
namaozi <cpshunya@gmail.com>
2018-01-08 23:06:35 +0900

    :sparkles: Add vue-router sample

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,39 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
+// Laravelオススメの初期化設定
 require('./bootstrap');
 
-window.Vue = require('vue');
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import ExampleComponent from './components/ExampleComponent.vue';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+import Foo from './components/Foo.vue';
+import Bar from './components/Bar.vue';
 
-const app = new Vue({
-    el: '#app'
+// ルート定義
+// 各ルートは 1 つのコンポーネントとマッピングされる必要があります。
+const routes = [{
+        path: '/foo',
+        component: Foo
+    },
+    {
+        path: '/bar',
+        component: Bar
+    },
+    {
+        path: '/example',
+        component: ExampleComponent
+    }
+];
+
+// 3. ルーターインスタンスを作成して、ルートオプションを渡します
+const router = new VueRouter({
+    routes // `routes: routes` の短縮表記
 });
+
+// 4. root となるインスタンスを作成してマウントします
+// アプリケーション全体がルーターを認知できるように、
+// ルーターをインジェクトすることを忘れないでください。
+const root = new Vue({
+    router
+}).$mount('#root');
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .browserSync('http://192.168.56.20/');
